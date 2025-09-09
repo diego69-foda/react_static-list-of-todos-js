4ae324bd5221bf01472b590118955450a7193a58
diego69-foda <diego.bkuhn@gmail.com>
2025-09-09 15:37:29 -0300

    atividade meia-bomba

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,10 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = (todos, users) => (
+  <section className="TodoList">
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">{todos.title}</h2>
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        {users[todos.userId - 1].name}
+      </a>
+    </article>
+  </section>
+);
